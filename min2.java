cb848aac7f28e55aaade1172a9aee70f1504a8b1
NanamiTo <114852541+NanamiTo@users.noreply.github.com>
2024-04-30 10:14:17 +0530

    Update min2.java

--- min2.java
+++ min2.java
@@ -2,7 +2,7 @@ public class min2 {
   public static void main(String[] args) {
      int num1 = 5;
      int num2 = 7;
-     int sum = num1 +num2[D[D[D[D [C[C[C[D[D[Dnum2;
+     int sum = num1 +num2;
      System.out.println("Sum of" + num1 + "+" + num2 + " is: " +sum);
     }
 }
