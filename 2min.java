4151f8e902fa34a2b5b2d6982e4fafad11554417
Aarushi Bhogate <aarushibhogate@gmail.com>
2024-04-30 09:55:48 +0530

    first commit

---
+++ 2min.java
@@ -0,0 +1,11 @@
+
+
+
+public class 2min {
+  public static void main(String[] args) {
+     int num1 = 5;
+     int num2 = 7;
+     int sum = num1 +num2[D[D[D[D [C[C[C[D[D[Dnum2;
+     System.out.println("Sum of" + num1 + "+" + num2 + " is: " +sum);
+    }
+}
